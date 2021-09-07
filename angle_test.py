1b8f1cbbda1aab7bd965950f5e0386a3ec83e02e
taatya <taatya@githib.com>
2021-09-07 18:05:35 +0530

    tried fixing it not working yet tho

--- angle_test.py
+++ angle_test.py
@@ -1,41 +1,29 @@
 # Imports
 import turtle
-from utilities import draw_left_curves, draw_right_curves, Point, getCoordsForXAxis, getCoordsForYAxis, pen_speed
+from utu2 import screen_size, width, height, padding, paddedWidth, paddedHeight, origin, yMax, xMax, negativeXMax, negativeYMax, topLeft, topRight, bottomLeft, bottomRight, Point, getCoordsForYAxis, getCoordsForXAxis, draw_right_curves, draw_left_curves
+
+print("starting parabola drawing")
 
 
-# Screen Size
-screen_size = 1000.0
-width = screen_size
-height = screen_size
-padding = 40.0
-paddedWidth = width - padding
-paddedHeight = height - padding
-
-# Points
-origin = Point(((screen_size - padding) / 2) * (-1),((screen_size - padding) / 2) * (-1))
-yMax = Point(origin.x, paddedHeight / 2)
-xMax = Point(paddedWidth/ 2, origin.y)
-negativeXMax = Point((paddedWidth/ 2) * (-1), origin.y)
-negativeYMax = Point(origin.x, (paddedHeight/ 2) * (-1))
-topLeft = Point(negativeXMax.x, yMax.y)
-topRight = Point(xMax.x, yMax.y)
-bottomLeft = Point(negativeXMax.x, negativeYMax.y)
-bottomRight = Point(xMax.x, negativeYMax.y)
 
 # Parts
 try:
     parts = float(input('Please enter how many parts you would like ( the number of dots): '))
     parts += 1
-except:
+except ValueError:
     print('You did not type a number! Please re-start the program, and do it right this time')
     quit()
 
 # Steps
 steps = (yMax.y - origin.y) / parts
 
-yList = getCoordsForYAxis(origin, Point(origin.x, yMax.y), steps, origin.x)
-xList = getCoordsForXAxis(origin, Point(xMax.x, origin.x), steps, origin.y)
+# Calculating coordinates
+yList = getCoordsForYAxis(origin, yMax, steps, 0)
+xList = getCoordsForXAxis(origin, xMax, steps, 0)
+negativeXList = getCoordsForXAxis(negativeXMax, origin, steps, 0)
+negativeYList = getCoordsForYAxis(negativeYMax, origin, steps, 0)
 yList.reverse()
+negativeXList.reverse()
 
 
 # Setting up the screen
@@ -43,17 +31,18 @@ wn = turtle.Screen()
 wn.setup(width=width, height=height)
 wn.tracer(1)
 wn.bgcolor('black')
-wn.title('Parabolic Curves (curve)')
+wn.title('Parabolic Curves')
 
 # Pen
 pen = turtle.Turtle()
-pen.speed(pen_speed)
+pen.speed(10)
 pen.color('green')
 pen.hideturtle()
 
+
 # Drawing the base
 pen.penup()
-pen.goto(origin.x, origin.y)
+pen.goto(0, 0)
 pen.pendown()
 pen.goto(origin.x, yMax.y)
 
@@ -62,8 +51,22 @@ pen.goto(origin.x, origin.y)
 pen.pendown()
 pen.goto(xMax.x, origin.y)
 
-# Making the curve
+pen.penup()
+pen.goto(origin.x, origin.y)
+pen.pendown()
+pen.goto(negativeXMax.x, origin.y)
+
+pen.penup()
+pen.goto(origin.x, origin.y)
+pen.pendown()
+pen.goto(origin.x, negativeYMax.y)
+
+
+# Making the curves
 draw_left_curves(pen, yList, xList)
+# draw_left_curves(pen, yList, negativeXList)
+# draw_left_curves(pen, negativeYList, negativeXList)
+# draw_left_curves(pen, negativeYList, xList)
 
 # Quitting the program
 running = True
@@ -76,8 +79,6 @@ def quit():
 wn.listen()
 wn.onkeypress(quit, 'q')
 wn.onkeypress(quit, 'Q')
-
-print("Working On It.")
 
 # Main Loop
 while running:
---
+++ utu2.py
@@ -0,0 +1,94 @@
+# Imports
+import turtle
+
+pen_speed = 10.0
+
+
+# Defines a coordinate
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return "(%d, %d)" % (self.x, self.y)
+    def __repr__(self):
+        return "(%d, %d)" % (self.x, self.y)
+
+class AnglePoint:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return "(%d, %d)" % (self.x, self.y)
+    def __repr__(self):
+        return "(%d, %d)" % (self.x, self.y)
+
+
+
+
+def draw_left_curves(pen, firstList, secondList):
+    i = 0
+    LenOfList = len(firstList)
+    while i < LenOfList:
+        pen.penup()
+        if i != len(firstList) - 1 and len(secondList) -1:
+            pen.goto(firstList[i+1].x, firstList[i].y)
+            pen.pendown()
+            pen.goto(secondList[i+1].x, secondList[i].y)
+        else:
+            pen.goto(firstList[i].x, firstList[i].y)
+            pen.pendown()
+            pen.goto(secondList[i].x, secondList[i].y)
+        i += 1
+def draw_right_curves(pen, firstList, secondList):
+    i = 0
+    LenOfList = len(firstList)
+    while i < LenOfList:
+        pen.penup()
+        if i != len(firstList) - 1 and len(secondList) -1:
+            pen.goto(firstList[i].x, firstList[i+1].y)
+            pen.pendown()
+            pen.goto(secondList[i].x, secondList[i+1].y)
+        else:
+            pen.goto(firstList[i].x, firstList[i].y)
+            pen.pendown()
+            pen.goto(secondList[i].x, secondList[i].y)
+        i += 1
+
+
+def getCoordsForXAxis(left, right, steps, y):
+    xList = []
+    x = left.x
+    while x <= right.x:
+        xPoint = Point(x, y)
+        xList.append(xPoint)
+        x += steps
+    return xList
+
+def getCoordsForYAxis(bottom, top, steps, x):
+    yList = []
+    y = bottom.y
+    while y <= top.y:
+        yPoint = Point(x, y)
+        yList.append(yPoint)
+        y += steps
+    return yList
+
+# Screen Size
+screen_size = 1000.0
+width = screen_size
+height = screen_size
+padding = 40.0
+paddedWidth = width - padding
+paddedHeight = height - padding
+
+# Points
+origin = Point(0, 0)
+yMax = Point(origin.x, paddedHeight / 2)
+xMax = Point(Point(960, -150), origin.y)
+negativeXMax = Point((paddedWidth/ 2) * (-1), origin.y)
+negativeYMax = Point(origin.x, (paddedHeight/ 2) * (-1))
+topLeft = Point(negativeXMax.x, yMax.y)
+topRight = Point(xMax.x, yMax.y)
+bottomLeft = Point(negativeXMax.x, negativeYMax.y)
+bottomRight = Point(xMax.x, negativeYMax.y)
